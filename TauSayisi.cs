2d6b25e14bdc12ce3b5a7acf6b33c08cd4512fe9
Hatice Özdilek <48315417+haticeozdilek@users.noreply.github.com>
2019-12-02 16:39:17 +0300

    Add files via upload

---
+++ TauSayisi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TauSayisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0, i = 1;
+            int sayi;
+
+            Console.WriteLine("Lütfen bir sayi giriniz :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            while (i != sayi + 1)
+            {
+                if (sayi % i == 0)
+                    sayac++;
+                i++;
+            }
+            if (sayi % sayac == 0)
+                Console.WriteLine("Girilen sayi Tau Sayisidir");
+            else
+                Console.WriteLine("Tau sayisi degildir.");
+        }
+       
+    }
+
+}
