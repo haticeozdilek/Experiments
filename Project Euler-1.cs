762e7968f55a5d8fd9f9e3509ae89517fa33e623
Hatice Özdilek <48315417+haticeozdilek@users.noreply.github.com>
2019-12-09 21:49:28 +0300

    Add files via upload

---
+++ Project Euler-1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int toplam = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    toplam+=i;
+                }    
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+       
+    }
+
+}
